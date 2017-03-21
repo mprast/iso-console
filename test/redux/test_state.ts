c1c9d1b61f99ec3539f61ce9b8389ca42a2286e9
Matt Prast <mprast@gmail.com>
2017-03-20 19:57:47 -0700

    :hammer: make test state helper build new state
    Since we've changed so many of our methods to mutate data (as a
    convenience) testing input v. output requires two copies of the
    test state. This changes getTestState(), which closes over a reference,
    to buildTestState(), which constrcuts a new state tree each time.

---
+++ test/redux/test_state.ts
@@ -0,0 +1,54 @@
+import {IsoState} from "src/redux/state_types";
+import {buildNode} from "src/redux/value_objects/node";
+
+export function buildTestState(): IsoState {
+    const nodes = [
+        {
+            name: "testOne",
+            isRoot: false,
+            incidents: ["testTwo, testThree"],
+            coords: {
+                x: 0,
+                y: 0,
+            },
+        },
+        {
+            name: "testTwo",
+            isRoot: true,
+            incidents: ["testThree"],
+            coords: {
+                x: 0,
+                y: 0,
+            },
+        },
+        {
+            name: "testThree",
+            incidents: ["testTwo"],
+            isRoot: false,
+            coords: {
+                x: 0,
+                y: 0,
+            },
+        },
+        {
+            name: "testFour",
+            incidents: ["testOne"],
+            isRoot: false,
+            coords: {
+                x: 0,
+                y: 0,
+            },
+        },
+    ];
+
+    const wrappedNodes = nodes.map((nodeObj) => buildNode(nodeObj));
+
+    const testState: IsoState = {
+        console: {
+            nodes: wrappedNodes,
+        },
+        admin: {},
+    };
+
+    return testState;
+};
