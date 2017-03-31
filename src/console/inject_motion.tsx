b6670f6153e0fd55c7ec79bd8decfcf0b4217fe0
Matt Prast <mprast@gmail.com>
2017-03-31 16:06:07 -0700

    :art: refactor injectMotion

--- src/console/inject_motion.tsx
+++ src/console/inject_motion.tsx
@@ -2,6 +2,7 @@ import { ComponentTree } from "src/redux/state_types";
 import { Node } from "src/console/components/node";
 import traverse from "react-traverse";
 import * as React from "react";
+import { Component, ReactElement } from "react";
 import {Motion, spring} from "react-motion";
 
 export function injectMotion(tree: ComponentTree): ComponentTree {
@@ -9,22 +10,7 @@ export function injectMotion(tree: ComponentTree): ComponentTree {
         ComponentElement(path: any) {
             switch (path.node.type) {
                 case Node: {
-                    const [x, y] = [path.node.props.finalX, path.node.props.finalY];
-                    return <Motion style={{x: spring(x), y: spring(y)}}>
-                        {
-                            (position: any) => {
-                                return React.cloneElement(
-                                    path.node,
-                                    Object.assign(
-                                        {},
-                                        path.node.props,
-                                        {x: position.x, y: position.y},
-                                    ),
-                                    ...path.traverseChildren(),
-                                );
-                            }
-                        }
-                    </Motion>;
+                    return wrapMotion(path.node, path.traverseChildren);
                 }
                 default: {
                     return path.traverseChildren();
@@ -32,4 +18,23 @@ export function injectMotion(tree: ComponentTree): ComponentTree {
             }
         },
     }) as ComponentTree;
+}
+
+function wrapMotion(node: ReactElement<any>, traverseChildren: () => ReactElement<any>): JSX.Element {
+    const [x, y] = [node.props.leftOffset, node.props.topOffset];
+    return <Motion style={{x: spring(x), y: spring(y)}}>
+        {
+            (position: any) => {
+                return React.cloneElement(
+                    node,
+                    Object.assign(
+                        {},
+                        node.props,
+                        {leftOffset: position.x, topOffset: position.y},
+                    ),
+                    traverseChildren(),
+                );
+            }
+        }
+        </Motion>;
 }
