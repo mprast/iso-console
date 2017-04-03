563a09db5c7d3de2407cb528bffc17fff2e1c16d
Matt Prast <mprast@gmail.com>
2017-04-02 18:45:24 -0700

    :wrench: turn on source maps in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 var path = require("path");
 module.exports = {
     entry: path.resolve("./src/entry_point.tsx"),
+    devtool: "source-map",
     module: {
         loaders: [
-            { test: /\.tsx?$/, exclude: /node_modules/, loader: 'awesome-typescript-loader' },
+            { test: /\.tsx?$/, exclude: /node_modules/, loader: "awesome-typescript-loader" },
             { 
               test: /\.css$/,
               exclude: /node_modules/,
-              loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]&camelCase=dashes'
+              loader: "style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]&camelCase=dashes"
             }
         ]
     },
